d1b64df199e9a01b237d6898b39449765aea9ccc
Rafael Sanchez <paocomcarnemoidaetubaina@gmail.com>
2023-08-24 08:52:19 -0300

    tutorial1

--- main.asm
+++ main.asm
@@ -16,3 +16,40 @@ WaitVBlank:
     ; Turn the LCD off
     ld a, 0
     ld [rLCDC], a
+
+    ; Copy the tile data
+    ld de, Tiles
+    ld hl, $9000
+    ld bc, TilesEnd - Tiles
+CopyTiles:
+    ld a, [de]
+    ld [hli], a
+    inc de
+    dec bc
+    ld a, b
+    or a, c
+    jp nz, CopyTiles
+
+    ; Copy the tilemap
+    ld de, Tilemap
+    ld hl, $9800
+    ld bc, TilemapEnd - Tilemap
+CopyTilemap:
+    ld a, [de]
+    ld [hli], a
+    inc de
+    dec bc
+    ld a, b
+    or a, c
+    jp nz, CopyTilemap
+
+    ; Turn the LCD on
+    ld a, LCDCF_ON | LCDCF_BGON
+    ld [rLCDC], a
+
+    ; During the first (blank) frame, initialize display registers
+    ld a, %11100100
+    ld [rBGP], a
+
+Done:
+    jp Done
